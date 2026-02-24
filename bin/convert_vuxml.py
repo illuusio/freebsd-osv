b58672a10d7509750358824738ef268ed920ed74
Tuukka Pasanen <tuukka.pasanen@ilmi.fi>
2026-02-24 14:25:54 +0200

    bin/convert_vuxml.py: Update licensening information
    Update licesening information to bin/convert_vuxml.py

--- bin/convert_vuxml.py
+++ bin/convert_vuxml.py
@@ -1,15 +1,37 @@
 #!/usr/bin/env python
 # SPDX-License-Identifier: BSD-2-Clause
 #
-# Copyright (c) 2025 The FreeBSD Foundation
+# Copyright (C) 1994-2024 The FreeBSD Project.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL AUTHOR OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+# Copyright (c) 2024 The FreeBSD Foundation
 #
 # Portions of this software were developed by Pierre Pronchery
 # <pierre@defora.net> at Defora Networks GmbH under sponsorship
 # from the FreeBSD Foundation.
 #
 # Portions of this software were developed by Tuukka Pasanen
-# <tuukka.pasanen@ilmi.fi> at Defora Networks GmbH under sponsorship
-# from the FreeBSD Foundation.
+# <tuukka.pasanen@ilmi.fi> under sponsorship from the FreeBSD Foundation.
 
 """VuXML to OSV converter."""
 import datetime
