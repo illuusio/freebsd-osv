d2b33cf4a28d616ca15100cfc399b584c14e8cd4
Tuukka Pasanen <tuukka.pasanen@ilmi.fi>
2026-08-05 10:41:59 +0300

    Update vulnerabilities 2026-08-05
    Update to current VuXML

--- bin/convert_vuxml.py
+++ bin/convert_vuxml.py
@@ -48,7 +48,7 @@ re_date = re.compile(r"^(19|20)[0-9]{2}-[0-9]{2}-[0-9]{2}$")
 re_invalid_package_name = re.compile("[@!#$%^&*()<>?/\\|}{~:]")
 
 # warn if description has more than X characters
-DESCRIPTION_LENGTH = 28000
+DESCRIPTION_LENGTH = 64000
 
 namespace = "{http://www.vuxml.org/apps/vuxml-1}"
 
